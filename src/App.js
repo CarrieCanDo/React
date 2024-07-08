3e9b4d490cf901a7b51ebe8d17c20654dd65df17
CarrieCanDo <carrie.a.wright@outlook.com>
2024-07-08 18:42:35 -0400

    committing assignment 1 and 2

--- src/App.js
+++ src/App.js
@@ -18,23 +18,22 @@ function Square() {
 }
 
 export default function Board() {
-  const [squares, setSquares] = useState(Array(9)).fill(null)
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} />
-        <Square value={squares[1]} />
-        <Square value={squares[2]} />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} />
-        <Square value={squares[4]} />
-        <Square value={squares[5]} />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} />
-        <Square value={squares[7]} />
-        <Square value={squares[8]} />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
